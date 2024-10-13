62b76789153e04f4233fb9907e29c5c99ce064af
Le Trung Son <lesonlhld@gmail.com>
2024-10-13 16:25:30 +0700

    feat(Script): support update app name, package name and app icon for Flutter app

---
+++ README.md
@@ -0,0 +1,48 @@
+# Flutter app customizer script
+Shell script used to update app name, package name and app icon
+
+# Installation
+
+## 1. Git Submodule
+
+```bash
+git submodule add https://github.com/chungxon/flutter_app_customizer configs
+```
+
+## 2. Update file manually
+- Create a `configs` folder on your root project folder
+
+    ```bash
+    mkdir configs
+    ```
+
+- Here is the result
+
+    ```
+    my-app/ -> Root folder
+    ├─ android/
+    ├─ configs/ -> This is the custom folder
+    ├─ ios/
+    ├─ lib/
+    ```
+
+- Copy all files and folders of this project to `configs` folder.
+
+# Usage
+
+## 1. Setup your app configs
+- Open the `configs.props` file
+    - Update your app name
+    - Update your iOS bundle ID
+    - update your Android package name
+
+## 2. Update your app icon
+- Add your icon in `assets/icons/app_icon.png` or `configs/overrides/assets/icons/app_icon.png`
+- Or you can update the `image_path` in the `flutter_launcher_icons.yaml` file
+
+## 3. Run script
+Run the command
+
+```bash
+sh configs/app_customizer.sh
+```
---
+++ app_customizer.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+set -e
+
+red=$(tput setaf 1)
+green=$(tput setaf 2)
+none=$(tput sgr0)
+
+# Get working directory of the script
+cd "$(dirname "$0")"
+SCRIPT_DIR=$(pwd)
+# echo "Current directory: ${SCRIPT_DIR}"
+
+# Define configuration file
+CONFIG_FILE="${SCRIPT_DIR}/configs.props"
+
+# Define `load_properties` file path
+LOAD_PROPERTIES="${SCRIPT_DIR}/load_properties.sh"
+
+# Download load_properties script
+curl -s https://raw.githubusercontent.com/chungxon/load_properties/refs/heads/master/scripts/load_properties.sh -o ${LOAD_PROPERTIES}
+# echo "Load properties file downloaded: ${LOAD_PROPERTIES}"
+
+# Source the load_properties script
+source ${LOAD_PROPERTIES}
+
+# Load properties
+loadProperties ${CONFIG_FILE}
+
+# Define `overrides` directory
+OVERRIDES_DIR="${SCRIPT_DIR}/overrides"
+# echo "Overrides directory: ${OVERRIDES_DIR}"
+
+# Define root directory of the project
+PROJECT_DIR="$(dirname "$SCRIPT_DIR")"
+echo "Project directory: ${PROJECT_DIR}"
+
+# Override all files and folders in `overrides` folder with the ones in
+# `project` folder if they exist
+if [ -d "${OVERRIDES_DIR}" ]; then
+    echo "Copying "${OVERRIDES_DIR}" to project..."
+    cp -Rf "${OVERRIDES_DIR}/" "${PROJECT_DIR}/"
+    echo "Overriding done!"
+fi
+
+# Navigate to project folder
+cd "${PROJECT_DIR}"
+
+# Install `flutter_launcher_icons` package to generate app icons
+dart pub add dev:flutter_launcher_icons:"0.13.1"
+flutter pub get
+
+# Generating app icons
+dart run flutter_launcher_icons -f ${SCRIPT_DIR}/flutter_launcher_icons.yaml
+
+#------------------------------------------------------------------------------------------#
+# # Install `rename_app` package to rename app
+# flutter pub add dev:rename_app
+# flutter pub get
+
+# # Rename app
+# dart run rename_app:main all="${appName}" # android="Android Name" ios="IOS Name" web="Web Name" mac="Mac Name" windows="Windows Name" linux="Linux Name" others="Others Name"
+#------------------------------------------------------------------------------------------#
+
+#------------------------------------------------------------------------------------------#
+# # Install `change_app_package_name` package to change app package name
+# flutter pub add dev:change_app_package_name
+# flutter pub get
+
+# # Change package name
+# dart run change_app_package_name:main ${iosBundleId} --ios
+# dart run change_app_package_name:main ${androidPackageName} --android
+#------------------------------------------------------------------------------------------#
+
+#------------------------------------------------------------------------------------------#
+# Install `rename` package to rename app and change bundle ID
+flutter pub global activate rename
+flutter pub get
+
+# Rename app using `rename`
+rename setAppName --targets ios,android --value "${appName}" # --targets ios,android,macos,windows,linux,web
+
+# Change bundle ID
+rename setBundleId --targets ios --value ${iosBundleId}
+rename setBundleId --targets android --value ${androidPackageName}
+#------------------------------------------------------------------------------------------#
+
+# Clean up
+rm ${LOAD_PROPERTIES}
+
+# Print success message
+# Adjusting the output for better alignment
+line_width=80
+
+printf "\n"
+printf "%${line_width}s\n" | tr ' ' '='
+printf "* %-77s*\n" "App customization complete!"
+printf "* %-77s*\n" "- App name: ${appName}"
+printf "* %-77s*\n" "- Android package: ${androidPackageName}"
+printf "* %-77s*\n" "- iOS bundle ID: ${iosBundleId}"
+printf "* ${green}%-78s${none}*\n" "✅ All overrides have been applied."
+printf "%${line_width}s\n" | tr ' ' '='
+
+exit 0
